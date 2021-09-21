0565e88377d01e263584ecd98c1ae77cddba2294
LoganCoats22 <logancoats222@gmail.com>
2021-09-21 08:34:36 -0500

    Initial Commit

---
+++ Coats_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coats_Calculator
+//
+//  Created by Logan Coats on 9/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
